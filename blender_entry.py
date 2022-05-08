3ca334362639279e6f343592695743f50d39e7ba
Michael Belousov <mike.belousov@bentley.com>
2022-05-07 21:22:34 -0400

    fix sys path for blender addon

--- blender_entry.py
+++ blender_entry.py
@@ -1,1 +1,6 @@
-from .addon import *
+import sys
+# NOTE: REPL python seems to have a special sys.path entry of the empty string which probably means cwd
+# blender doesn't have it so we need to add it
+sys.path.insert(0, '')
+
+from addon import *
