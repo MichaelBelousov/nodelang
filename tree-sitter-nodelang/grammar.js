0e75d0da335a9b9f328a85fb174d84a3843af277
Michael Belousov <mike.belousov@bentley.com>
2022-12-31 23:30:01 -0500

    get parser generating

--- tree-sitter-nodelang/grammar.js
+++ tree-sitter-nodelang/grammar.js
@@ -1,95 +1,82 @@
-const ident_regex = /[a-zA-Z_][a-zA-Z0-9_]*/;
+// TODO: escapes
+const ident_regex = /([a-zA-Z_][a-zA-Z0-9_]*)|('[^']*')/;
+
+function commalist(l_delim, type, r_delim) {
+  return seq(l_delim, repeat(seq(type, ",")), optional(type), r_delim);
+}
 
 module.exports = grammar({
   name: "nodelang",
   rules: {
-    // NOTE: the first rule is the one is the top-level one!
-    source_file: ($) => repeat(choice($._stmt, $.rule)),
-    body: ($) => repeat1(choice($._stmt, $.rule)),
+    // NOTE: the first rule is the top-level one!
+    source_file: ($) => repeat(choice($._stmt)),
+    body: ($) => repeat1(choice($._stmt)),
+
     identifier: ($) => ident_regex,
-    // FIXME: probably need to allow escaping spaces or quoting or something
-    path: ($) => /[^\s]+/,
-    _stmt: ($) =>
-      choice(
-        $.assign,
-        $.append_assign,
-        $.expanding_assign,
-        $.diagnostic,
-        $.directive,
-        $.if
+    integer: ($) => /\d+/,
+    float: ($) => /\d+\.\d+/,
+    array: ($) => commalist("[", $._expr, "]"),
+    // NOTE: are there limitations to tree-sitter supporting this lookbehind? It hasn't complained yet...
+    string: ($) => /".*?(<!\\)"/m,
+    _literal: ($) => choice($.integer, $.float, $.array, $.string),
+
+    body: ($) => seq("{", repeat($._stmt), "}"),
+    group: ($) => seq("group", field("name", $.identifier), $.body),
+
+    _stmt: ($) => choice($.if, $.var_decl),
+
+    var_decl: ($) =>
+      seq(
+        "const",
+        field("name", $.identifier),
+        optional(seq(":", field("type", $._expr))),
+        "=",
+        field("value", $._expr)
       ),
 
     word: ($) => $.identifier,
 
-    // note this is overloaded with `appendMacroSeparator` special command
     assign: ($) =>
-      // NOTE: will have to manually evaluate `$()` in rest_of_line
-      seq(
-        field("identifier", $.identifier),
-        "=",
-        field("value", $.rest_of_line)
-      ),
+      seq(field("name", $.identifier), "=", field("value", $._expr)),
+
     append_assign: ($) =>
-      seq(
-        field("identifier", $.identifier),
-        "+",
-        field("value", $.rest_of_line)
-      ),
+      seq(field("identifier", $.identifier), "+=", field("value", $._expr)),
 
     comment: ($) => token(/#.*/),
 
-    else_clause: ($) => seq("%else", $.body),
-    elif_cond: ($) => $.rest_of_line,
-    elif_clause: ($) => seq("%elif", $.elif_cond, $.body),
-
-    if_cond: ($) => $._expr,
-    ifdef_cond: ($) => $.identifier,
-    iffile_cond: ($) => $.rest_of_line,
     if: ($) =>
       choice(
         seq(
-          field(
-            "cond",
-            choice(
-              seq("%if", $.if_cond),
-              seq("%ifdef", $.ifdef_cond),
-              seq("%ifndef", $.ifdef_cond),
-              seq("%iffile", $.iffile_cond),
-              seq("%ifnofile", $.iffile_cond)
-            )
-          ),
-          "\n",
-          $.body,
-          repeat($.elif_clause),
-          optional($.else_clause),
-          "%endif"
+          "if",
+          "(",
+          field("cond", $._expr),
+          ")",
+          field("then", $.body),
+          optional(seq("else", $.body))
         )
       ),
 
-    // TODO: definitely needs to have escapable slashes
-    string: ($) => /"[^"]*"/,
+    _arg: ($) =>
+      seq(
+        optional(seq(".", field("name", $.identifier), "=")),
+        field("value", $._expr)
+      ),
 
     // unary
     not: ($) => prec(1000, seq("!", $._expr)),
-    is_defined: ($) => prec(1000, seq("defined", "(", $.identifier, ")")),
+    _unary_expr: ($) => choice($.not),
 
     // binary
     eq: ($) => prec.left(2, seq($._expr, "==", $._expr)),
     or: ($) => prec.left(3, seq($._expr, "||", $._expr)),
     and: ($) => prec.left(4, seq($._expr, "&&", $._expr)),
+    call: ($) => prec.left(5, seq($._expr, commalist("(", $._arg, ")"))),
+    _binary_expr: ($) => choice($.eq, $.or, $.and, $.call),
 
     _expr: ($) =>
-      choice(
-        $.not,
-        $.is_defined,
-        $.eq,
-        $.or,
-        $.and,
-        $.string,
-        $.expand,
-        $.identifier
-      ),
+      choice($._binary_expr, $._unary_expr, $._literal, $.identifier),
   },
+
   // prettier-ignore
   extras: ($) => [
     $.comment,
